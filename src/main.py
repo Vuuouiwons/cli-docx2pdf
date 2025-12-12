764e3a73ec597163f4b7680194c02d8faf7dedb7
Vinsensius Reinard <vinconloren@gmail.com>
2025-12-12 23:49:19 +0700

    imprv: added types

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 import os
 from docx2pdf import convert
+from pathlib import Path
+
 
 from modules.argparser import parser
 from modules.Path import get_output_path
 
+
 def main():
     args = parser.parse_args()
     
-    input_docx_file = args.input_docx_file
+    input_docx_file: Path = args.input_docx_file
+    
     convert(input_docx_file, get_output_path())
 
 
--- src/modules/Path.py
+++ src/modules/Path.py
@@ -2,16 +2,16 @@ import platform
 from pathlib import Path
 
 def get_output_path() -> Path:
-    system = platform.system()
+    system: str = platform.system()
 
     if system == 'Windows':
-        output_path = Path("~/Desktop/DOCX2PDF").expanduser()
+        output_path: Path = Path("~/Desktop/DOCX2PDF").expanduser()
     elif system == 'Darwin':
-        output_path = Path("~/Desktop/DOCX2PDF").expanduser()
+        output_path: Path = Path("~/Desktop/DOCX2PDF").expanduser()
     elif system == 'Linux':
-        output_path = Path("~/DOCX2PDF").expanduser()
+        output_path: Path = Path("~/DOCX2PDF").expanduser()
     else:
-        output_path = Path("~").expanduser() / "DOCX2PDF"
+        output_path: Path = Path("~").expanduser() / "DOCX2PDF"
 
     output_path.mkdir(parents=True, exist_ok=True)
 
